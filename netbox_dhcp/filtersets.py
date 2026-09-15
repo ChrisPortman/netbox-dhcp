34286ce5106c1e9bb07fbbc45523292358b9b103
Chris Portman <cportman@iron.ore>
2026-09-15 11:45:19 +1000

    Add Additional Filters For Lease Objects
    Allow filtering on `client_id`, `hostname`, `expire_time` and `lease_time`.

--- netbox_dhcp/filtersets.py
+++ netbox_dhcp/filtersets.py
@@ -1,6 +1,11 @@
 from django.utils.translation import gettext as _
+from utilities.filters import (
+    MultiValueCharFilter,
+    MultiValueDateTimeFilter,
+    MultiValueMACAddressFilter,
+)
+
 from netbox.filtersets import NetBoxModelFilterSet
-from utilities.filters import MultiValueMACAddressFilter
 
 from .models import DhcpLease
 
@@ -11,6 +16,26 @@ class DhcpLeaseFilterSet(
     mac_address = MultiValueMACAddressFilter(
         field_name='mac_address',
         label=_('MAC address'),
+    )
+
+    client_id = MultiValueCharFilter(
+        field_name='client_id',
+        label=_('Client ID'),
+    )
+
+    hosthame = MultiValueCharFilter(
+        field_name='hostname',
+        label=_('Hostname'),
+    )
+
+    lease_time = MultiValueDateTimeFilter(
+        field_name='lease_time',
+        label=_('Lease Time'),
+    )
+
+    expire_time = MultiValueDateTimeFilter(
+        field_name='expire_time',
+        label=_('Expire Time'),
     )
 
     class Meta:
